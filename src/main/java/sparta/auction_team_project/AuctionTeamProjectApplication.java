e2cfebe2efcfb70ce414aeeb2da88de9a040bba4
김대훈 <eogns136@gmail.com>
2026-03-06 09:33:06 +0900

    chore: first commit

---
+++ src/main/java/sparta/auction_team_project/AuctionTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package sparta.auction_team_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionTeamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionTeamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sparta/auction_team_project/AuctionTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.auction_team_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionTeamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
