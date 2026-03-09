2d02c9355461a27091c93e64232c7e5d5f61df39
hhjo <43205471+hhjo96@users.noreply.github.com>
2026-03-09 19:31:06 +0900

    fix: baseResponse 응답 형식 수정

--- src/main/java/sparta/auction_team_project/domain/user/controller/UserController.java
+++ src/main/java/sparta/auction_team_project/domain/user/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import sparta.auction_team_project.common.dto.AuthUser;
+import sparta.auction_team_project.common.response.BaseResponse;
+import sparta.auction_team_project.domain.user.dto.request.UserChangeNicknameRequest;
 import sparta.auction_team_project.domain.user.dto.request.UserChangePasswordRequest;
 import sparta.auction_team_project.domain.user.dto.response.UserGetResponse;
 import sparta.auction_team_project.domain.user.service.UserService;
@@ -19,14 +21,15 @@ public class UserController {
 
     //마이페이지 조회
     @GetMapping("/me")
-    public ResponseEntity<UserGetResponse> getUser(@AuthenticationPrincipal AuthUser authUser) {
-        return ResponseEntity.ok(userService.getUser(authUser.getId()));
+    public BaseResponse<UserGetResponse> getUser(@AuthenticationPrincipal AuthUser authUser) {
+        return BaseResponse.success("200", "마이페이지 조회 성공", userService.getUser(authUser.getId()));
     }
 
     //비밀번호 변경
     @PatchMapping("/me/password")
-    public void changePassword(@AuthenticationPrincipal AuthUser authUser, @Valid @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
+    public BaseResponse<Void> changePassword(@AuthenticationPrincipal AuthUser authUser, @Valid @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
         userService.changePassword(authUser.getId(), userChangePasswordRequest);
+        return BaseResponse.success("200", "비밀번호 변경 성공", null);
     }
 
     //닉네임 변경
