4389026eb34842fb8aa3792b7db172085ae99ce7
jihyeon <yio1346@gmail.com>
2026-03-10 15:40:18 +0900

    feat: websocket 기본 설정파일 및 의존성 추가

--- src/main/java/sparta/auction_team_project/domain/chat/controller/ChatController.java
+++ src/main/java/sparta/auction_team_project/domain/chat/controller/ChatController.java
@@ -1,11 +1,19 @@
 package sparta.auction_team_project.domain.chat.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
-import sparta.auction_team_project.domain.chat.service.ChatService;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ChatController {
-    private final ChatService chatService;
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat.send")
+    public void sendMessage(String message, SimpMessageHeaderAccessor headerAccessor) {
+
+    }
 }
