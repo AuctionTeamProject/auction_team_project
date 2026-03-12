49f1101cf225dec58eb210f6a6de0f220349edf7
eNoLJ <derosatam76@gmail.com>
2026-03-12 16:36:47 +0900

    feat: 쿠폰 사용 API 구현

--- src/main/java/sparta/auction_team_project/domain/coupon/repository/CouponRepository.java
+++ src/main/java/sparta/auction_team_project/domain/coupon/repository/CouponRepository.java
@@ -3,5 +3,5 @@ package sparta.auction_team_project.domain.coupon.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sparta.auction_team_project.domain.coupon.entity.Coupon;
 
-public interface CouponRepository extends JpaRepository<Coupon, Long> {
+public interface CouponRepository extends JpaRepository<Coupon, Long>, CouponCustomRepository {
 }
