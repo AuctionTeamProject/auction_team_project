297c1286c980bc6b665ab6c59620448dc3dfcec4
hhjo <43205471+hhjo96@users.noreply.github.com>
2026-03-12 19:58:26 +0900

    fix: conflict

--- src/main/java/sparta/auction_team_project/domain/memberShip/entity/Membership.java
+++ src/main/java/sparta/auction_team_project/domain/memberShip/entity/Membership.java
@@ -3,9 +3,13 @@ package sparta.auction_team_project.domain.memberShip.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import sparta.auction_team_project.common.exception.ErrorEnum;
+import sparta.auction_team_project.common.exception.ServiceErrorException;
 import sparta.auction_team_project.domain.memberShip.enums.MembershipEnum;
+import sparta.auction_team_project.domain.user.enums.UserRole;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @Getter
 @Entity
@@ -17,10 +21,13 @@ public class Membership {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
     MembershipEnum grade;
 
     LocalDateTime expiredAt;
 
+    @Column(nullable = false)
     Long userId;
 
     public Membership(MembershipEnum grade, LocalDateTime expiredAt, Long userId) {
@@ -34,9 +41,12 @@ public class Membership {
     }
 
     public void extendPeriod(int days) {
-        if (this.expiredAt == null) {
-            this.expiredAt = LocalDateTime.now().plusDays(days);
-        } else {
+        LocalDateTime now = LocalDateTime.now();
+
+        // 만료일이 널이거나 지난 경우
+        if (this.expiredAt == null || this.expiredAt.isBefore(now)) {
+            this.expiredAt = now.plusDays(days);
+        } else { // 만료일이 안지난경우
             this.expiredAt = this.expiredAt.plusDays(days);
         }
     }
