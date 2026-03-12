edf7afe5888fdcc4cd4ba490c4dca963b5d55822
EUNSIK JUNG <bamtol2100@naver.com>
2026-03-12 20:34:21 +0900

    test: 경매 스케줄러 단위테스트

---
+++ src/test/java/sparta/auction_team_project/domain/auction/scheduler/AuctionSchedulerTest.java
@@ -0,0 +1,55 @@
+package sparta.auction_team_project.domain.auction.scheduler;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sparta.auction_team_project.domain.auction.entity.Auction;
+import sparta.auction_team_project.domain.auction.repository.AuctionRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class AuctionSchedulerTest {
+
+    @Mock
+    private AuctionRepository auctionRepository;
+
+    @Mock
+    private Auction auction;
+
+    private AuctionScheduler auctionScheduler;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        auctionScheduler = new AuctionScheduler(auctionRepository);
+    }
+
+    @Test
+    void 시작10분전_승인되지않은경매_자동취소() {
+
+        when(auctionRepository.findPendingAuctionsBeforeStart(any(LocalDateTime.class)))
+                .thenReturn(List.of(auction));
+
+        auctionScheduler.cancelPendingAuctions();
+
+        verify(auction).cancel();
+    }
+
+    @Test
+    void 시작시간이된_READY경매_자동시작() {
+
+        when(auctionRepository.findReadyAuctionsToStart(any(LocalDateTime.class)))
+                .thenReturn(List.of(auction));
+
+        auctionScheduler.startReadyAuctions();
+
+        verify(auction).startAuction();
+    }
+}
