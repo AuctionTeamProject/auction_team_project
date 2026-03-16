5338b7541f22f0937ef1ff355b8b33b03b75e189
jihyeon <yio1346@gmail.com>
2026-03-16 14:43:48 +0900

    chore: 불필요한 import 제거

--- src/main/java/sparta/auction_team_project/common/dto/BidPlacedEvent.java
+++ src/main/java/sparta/auction_team_project/common/dto/BidPlacedEvent.java
@@ -5,13 +5,10 @@ import lombok.Getter;
 @Getter
 public class BidPlacedEvent {
 
-    // 경매 ID
     private final Long auctionId;
 
-    // 새로 최고 입찰자가 된 사용자
     private final Long bidderId;
 
-    // 이전 최고 입찰자 (OUT_BID 알림 대상)
     private final Long previousTopBidderId;
 
     public BidPlacedEvent(Long auctionId, Long bidderId, Long previousTopBidderId) {
