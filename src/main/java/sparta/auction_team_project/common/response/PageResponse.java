45172f10894e20f46d6bf5313c11d849f31a442e
EUNSIK JUNG <bamtol2100@naver.com>
2026-03-20 20:54:03 +0900

    feat: 목록 조회 v2 Redis Cache 적용

--- src/main/java/sparta/auction_team_project/common/response/PageResponse.java
+++ src/main/java/sparta/auction_team_project/common/response/PageResponse.java
@@ -1,11 +1,15 @@
 package sparta.auction_team_project.common.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PageResponse<T> {
 
     private List<T> content;
